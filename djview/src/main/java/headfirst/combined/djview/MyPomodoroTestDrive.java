478f03e759eb9e538aa57f06107eb63df39097df
MiaSinger <mia4444@hotmail.de>
2015-06-28 22:19:52 -0300

    diagrama de clases agregado

--- djview/src/main/java/headfirst/combined/djview/MyPomodoroTestDrive.java
+++ djview/src/main/java/headfirst/combined/djview/MyPomodoroTestDrive.java
@@ -4,8 +4,8 @@ public class MyPomodoroTestDrive {
 	
 	public static void main (String[] args){
 	
-	TimerModelInterface timer = new PomodoroModel();
-	ControllerInterface timercontroller = new PomodoroController(timer);
+	//TimerModelInterface timer = new PomodoroModel();
+	//ControllerInterface timercontroller = new PomodoroController(timer);
 	
 	TimerModelInterface timer2 = new PomodoroModel();
 	ControllerInterface timer2controller = new PomodoroControlleryVista(timer2);
